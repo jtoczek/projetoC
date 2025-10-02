ecc16ccbe08c5fa8065b8fe38cb9eb31ba4dfcc6
JONATHAN TOCZEK SOUZA <jtoczek@gmail.com>
2025-10-01 21:20:32 -0300

    Create ex1.c
    inseri

---
+++ ex1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+typedef struct {
+	int matricula;
+	char nome[30];
+	int idade;
+} TAluno;
+
+int qt=0;
+
+void insere(TAluno a[]){
+	 if (qt<30) {	 
+	 	 printf("\ninforme a matricula:");
+	 	 scanf("%d",&a[qt].matricula);
+	 	 printf("informe o a idade:");
+	 	 scanf("%d", &a[qt].idade);
+	 	 printf("informe o nome:");
+	 	 scanf("%s", a[qt].nome);
+	 	 qt++;
+	} else 
+		printf("\no vetor esta cheio!");	
+}
+
+void exibe(TAluno a[]){
+	int i;
+	for (i=0;i<qt;i++) {
+		printf("\n\n(%d) %s", a[i].matricula, a[i].nome);
+	}
+}
+
+
+int main(void) {
+TAluno a[30];
+int op=0;
+	do {
+		printf("\n*************************************************\n 1 - insere \n 2 - exibe \n 3 - sair \n informe a opcao:");
+		scanf("%d",&op);
+		
+		switch (op) {
+			case 1: 
+				insere(a);
+				break;
+			case 2: 
+				exibe(a);
+				break;
+			case 3:
+				printf("\nfim");
+				break;
+			default: 
+				printf("informe uma opcao valida!");
+				break;
+		}
+	} while(op!=3);
+
+	return 0;
+}
